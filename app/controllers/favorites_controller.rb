ae376b8634f98abc9c2f8301bf68ae7684e256a2
Maitake_Seven <daisukevita108@gmail.com>
2020-04-20 12:12:58 +0000

    v1.3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.fav_in(micropost)
     flash[:success] = 'お気に入り登録しました。'
-    redirect_to likes_user_path
+    redirect_to likes_user_path(current_user.id)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.fav_out(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to likes_user_path
+    redirect_to likes_user_path(current_user.id)
   end
 end
